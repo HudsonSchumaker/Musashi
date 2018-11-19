2b160ec71c7f325dbc560e2860cc2a0bd8ee2e50
Hudson Luiz Sales Schumaker <provider.hschumaker@terceiros.mongeralaegon.com.br>
2018-11-19 09:35:34 -0200

    SUPPORTED_FILES xls, xlsx, ppt, pptx

--- src/de/com/schumaker/core/HsSupportedFiles.java
+++ src/de/com/schumaker/core/HsSupportedFiles.java
@@ -12,9 +12,13 @@ import de.com.schumaker.core.files.impl.HsJava;
 import de.com.schumaker.core.files.impl.HsJs;
 import de.com.schumaker.core.files.impl.HsJsp;
 import de.com.schumaker.core.files.impl.HsPdf;
+import de.com.schumaker.core.files.impl.HsPpt;
+import de.com.schumaker.core.files.impl.HsPptx;
 import de.com.schumaker.core.files.impl.HsRtf;
 import de.com.schumaker.core.files.impl.HsTxt;
 import de.com.schumaker.core.files.impl.HsXhtml;
+import de.com.schumaker.core.files.impl.HsXls;
+import de.com.schumaker.core.files.impl.HsXlsx;
 import java.util.HashMap;
 
 /**
@@ -27,19 +31,23 @@ public class HsSupportedFiles {
     private static final HashMap<String, HsReadable> SUPPORTED_FILES = new HashMap<>();
 
     static {
-//        SUPPORTED_FILES.put(MusashiExtensions.HTML,  new HsHtml());
-//        SUPPORTED_FILES.put(MusashiExtensions.XHTML, new HsXhtml());
-//        SUPPORTED_FILES.put(MusashiExtensions.HTM,   new HsHtm());
-//        SUPPORTED_FILES.put(MusashiExtensions.CSS,   new HsCss());
-//        SUPPORTED_FILES.put(MusashiExtensions.JS,    new HsJs());
-//        SUPPORTED_FILES.put(MusashiExtensions.JAVA,  new HsJava());
-//        SUPPORTED_FILES.put(MusashiExtensions.JSP,   new HsJsp());
-//        SUPPORTED_FILES.put(MusashiExtensions.CS,    new HsCs());
-//        SUPPORTED_FILES.put(MusashiExtensions.TXT,   new HsTxt());
-//        SUPPORTED_FILES.put(MusashiExtensions.RTF,   new HsRtf());
+        SUPPORTED_FILES.put(MusashiExtensions.HTML,  new HsHtml());
+        SUPPORTED_FILES.put(MusashiExtensions.XHTML, new HsXhtml());
+        SUPPORTED_FILES.put(MusashiExtensions.HTM,   new HsHtm());
+        SUPPORTED_FILES.put(MusashiExtensions.CSS,   new HsCss());
+        SUPPORTED_FILES.put(MusashiExtensions.JS,    new HsJs());
+        SUPPORTED_FILES.put(MusashiExtensions.JAVA,  new HsJava());
+        SUPPORTED_FILES.put(MusashiExtensions.JSP,   new HsJsp());
+        SUPPORTED_FILES.put(MusashiExtensions.CS,    new HsCs());
+        SUPPORTED_FILES.put(MusashiExtensions.TXT,   new HsTxt());
+        SUPPORTED_FILES.put(MusashiExtensions.RTF,   new HsRtf());
         SUPPORTED_FILES.put(MusashiExtensions.PDF,   new HsPdf());
         SUPPORTED_FILES.put(MusashiExtensions.DOC,   new HsDoc());
         SUPPORTED_FILES.put(MusashiExtensions.DOCX,  new HsDocx());
+        SUPPORTED_FILES.put(MusashiExtensions.XLS,   new HsXls());
+        SUPPORTED_FILES.put(MusashiExtensions.XLSX,  new HsXlsx());
+        SUPPORTED_FILES.put(MusashiExtensions.PPT,   new HsPpt());
+        SUPPORTED_FILES.put(MusashiExtensions.PPTX,  new HsPptx());
     }
 
     private HsSupportedFiles() { }
