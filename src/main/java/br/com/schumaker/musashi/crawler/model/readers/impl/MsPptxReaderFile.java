1da6c2fceaa2611416d8e3c59d62d36b8b8c6bb5
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-06 00:14:00 +0100

    Added MsPptxReaderFile.

---
+++ src/main/java/br/com/schumaker/musashi/crawler/model/readers/impl/MsPptxReaderFile.java
@@ -0,0 +1,59 @@
+package br.com.schumaker.musashi.crawler.model.readers.impl;
+
+import br.com.schumaker.musashi.crawler.model.readers.MsReader;
+import org.apache.poi.hslf.extractor.QuickButCruddyTextExtractor;
+import org.apache.poi.ooxml.POIXMLProperties;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.apache.poi.xslf.usermodel.XSLFTextShape;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+public class MsPptxReaderFile implements MsReader{
+
+    @Override
+    public String read(String filePath) {
+        return readPptxFile(filePath);
+    }
+
+    private String readPptxFile(String filePath){
+        StringBuilder strBuilder = new StringBuilder();
+        File file = new File(filePath);
+        FileInputStream fis = null;
+        XMLSlideShow pptx = null;
+        try {
+            fis = new FileInputStream(file.getAbsolutePath());
+            pptx = new XMLSlideShow(fis);
+
+            POIXMLProperties.CoreProperties props = pptx.getProperties().getCoreProperties();
+            strBuilder.append(props.getTitle().trim());
+
+            for (XSLFSlide slide : pptx.getSlides()) {
+                List<XSLFShape> shapes = slide.getShapes();
+                for (XSLFShape shape : shapes) {
+                    if (shape instanceof XSLFTextShape) {
+                        XSLFTextShape textShape = (XSLFTextShape) shape;
+                        strBuilder.append(textShape.getText().trim());
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println("HsPptxReaderFile.readPptxFile: Exception " + filePath + "\n" + ex.getMessage());
+        }finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (pptx != null) {
+                    pptx.close();
+                }
+            } catch (Exception ex) {
+                System.err.println("HsPptxReaderFile.readPptxFile: Exception " + filePath + "\n" + ex.getMessage());
+            }
+        }
+        return strBuilder.toString();
+    }
+}
