a31d5a4106c8d1e27fd6726777326b6fa582bd67
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-06 00:07:03 +0100

    Changed MsTxt.

--- src/main/java/br/com/schumaker/musashi/crawler/model/files/MsTxt.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/files/MsTxt.java
@@ -3,12 +3,16 @@ package br.com.schumaker.musashi.crawler.model.files;
 import br.com.schumaker.musashi.crawler.model.MsFile;
 import br.com.schumaker.musashi.crawler.model.MsReadable;
 import br.com.schumaker.musashi.crawler.model.readers.impl.MsBasicReaderFile;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Hudson Schumaker
  */
 public class MsTxt extends MsFile implements MsReadable {
+
+    @Autowired
+    private MsBasicReaderFile reader;
 
     public MsTxt() {}
 
@@ -23,11 +27,11 @@ public class MsTxt extends MsFile implements MsReadable {
 
     @Override
     public String read() {
-        return new MsBasicReaderFile().read(getPath());
+        return reader.read(getPath());
     }
 
     @Override
     public MsReadable newOne(String path, String name, String ext) {
-        return null;
+        return new MsTxt(path, name, ext);
     }
 }
