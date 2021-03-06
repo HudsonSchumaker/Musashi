8f927b772a99fe1fe54b66b82a43c7d09bd5c3e2
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-07 00:25:54 +0100

    Added mappers.

---
+++ src/main/java/br/com/schumaker/musashi/crawler/service/mappers/MsFile2MsDbFile.java
@@ -0,0 +1,23 @@
+package br.com.schumaker.musashi.crawler.service.mappers;
+
+import br.com.schumaker.musashi.crawler.model.MsFile;
+import br.com.schumaker.musashi.crawler.model.db.MsDbFile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MsFile2MsDbFile implements Mapper<MsFile, MsDbFile> {
+
+    @Override
+    public MsDbFile from(MsFile source) {
+        MsDbFile target = new MsDbFile();
+        map(source, target);
+        return target;
+    }
+
+    @Override
+    public void map(MsFile source, MsDbFile target) {
+        target.setName(source.getName());
+        target.setContent(source.getContent());
+        target.setExt(source.getExt());
+    }
+}
