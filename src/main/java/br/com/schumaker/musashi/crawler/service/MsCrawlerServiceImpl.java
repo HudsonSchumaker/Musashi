889968f6bcfc4d2a5e80cf002d0415a3622d83c1
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-07 00:32:44 +0100

    Added MsCrawlerServiceImpl.

---
+++ src/main/java/br/com/schumaker/musashi/crawler/service/MsCrawlerServiceImpl.java
@@ -0,0 +1,44 @@
+package br.com.schumaker.musashi.crawler.service;
+
+import br.com.schumaker.musashi.crawler.model.db.repositories.MsDbFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+@Service
+public class MsCrawlerServiceImpl implements MsCrawlerService {
+
+    @Autowired
+    private MsDbFileRepository repository;
+
+    private final ExecutorService directories;
+    private final ExecutorService files;
+
+    public MsCrawlerServiceImpl() {
+        this.directories = Executors.newFixedThreadPool(256, new MsThreadFactory());
+        this.files = Executors.newFixedThreadPool(64, new MsThreadFactory());
+    }
+
+    @Override
+    public void startCrawler(String path) {
+        File file = new File(path);
+        File []roots = file.listFiles();
+
+        for (File f : roots) {
+            if (f.isDirectory()) {
+                Crawler c = new Crawler(f.getAbsolutePath(), files, repository);
+                this.directories.execute(c);
+            } else {
+                Crawler c = new Crawler(f.getAbsolutePath(), files, repository);
+                this.files.execute(c);
+            }
+        }
+    }
+}
