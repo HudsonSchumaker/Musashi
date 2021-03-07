5979fae2d8d1a78a45652a74ce36aa6d86701797
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-07 11:14:11 +0100

    Added MsSearchDTO.

---
+++ src/main/java/br/com/schumaker/musashi/search/model/MsSearchDTO.java
@@ -0,0 +1,11 @@
+package br.com.schumaker.musashi.search.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MsSearchDTO {
+
+    private String query;
+}
