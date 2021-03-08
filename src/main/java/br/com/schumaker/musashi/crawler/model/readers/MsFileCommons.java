785aa8250abd354b8fa2d88bc707c981edd4f4fd
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:57:40 +0100

    Added MsFileCommons.

---
+++ src/main/java/br/com/schumaker/musashi/crawler/model/readers/MsFileCommons.java
@@ -0,0 +1,8 @@
+package br.com.schumaker.musashi.crawler.model.readers;
+
+public class MsFileCommons {
+    public static final int READ_SIZE = 6114;
+    public static final int FILE_MAX_SIZE = 8388609; // 8MB
+
+    private MsFileCommons() {}
+}
