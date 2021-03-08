c96ef62ce6bec7ffa1873cdc566a695aa1ac9600
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:53:06 +0100

    Changed MsRtf.

--- src/main/java/br/com/schumaker/musashi/crawler/model/files/MsRtf.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/files/MsRtf.java
@@ -3,16 +3,12 @@ package br.com.schumaker.musashi.crawler.model.files;
 import br.com.schumaker.musashi.crawler.model.MsFile;
 import br.com.schumaker.musashi.crawler.model.MsReadable;
 import br.com.schumaker.musashi.crawler.model.readers.impl.MsRtfReaderFile;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Hudson Schumaker
  */
 public class MsRtf extends MsFile implements MsReadable {
-
-    @Autowired
-    private MsRtfReaderFile reader;
 
     public MsRtf() {}
 
@@ -21,13 +17,14 @@ public class MsRtf extends MsFile implements MsReadable {
     }
 
     @Override
-    public void process() {
+    public MsFile process() {
         super.setContent(read());
+        return this;
     }
 
     @Override
     public String read() {
-        return reader.read(getPath());
+        return new MsRtfReaderFile().read(getPath());
     }
 
     @Override
