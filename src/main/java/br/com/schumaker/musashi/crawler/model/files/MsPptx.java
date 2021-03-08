32f7e89a1f59cebf73d499a7f5e270d623cca395
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:52:44 +0100

    Changed MsPptx.

--- src/main/java/br/com/schumaker/musashi/crawler/model/files/MsPptx.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/files/MsPptx.java
@@ -3,16 +3,12 @@ package br.com.schumaker.musashi.crawler.model.files;
 import br.com.schumaker.musashi.crawler.model.MsFile;
 import br.com.schumaker.musashi.crawler.model.MsReadable;
 import br.com.schumaker.musashi.crawler.model.readers.impl.MsPptxReaderFile;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Hudson Schumaker
  */
 public class MsPptx extends MsFile implements MsReadable {
-
-    @Autowired
-    private MsPptxReaderFile reader;
 
     public MsPptx() {}
 
@@ -21,13 +17,14 @@ public class MsPptx extends MsFile implements MsReadable {
     }
 
     @Override
-    public void process() {
+    public MsFile process() {
         super.setContent(read());
+        return this;
     }
 
     @Override
     public String read() {
-        return reader.read(getPath());
+        return new MsPptxReaderFile().read(getPath());
     }
 
     @Override
