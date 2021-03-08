ccad171bb6142aa098bc0377ef907a9da334ee32
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:50:59 +0100

    Changed MsPdf.

--- src/main/java/br/com/schumaker/musashi/crawler/model/files/MsPdf.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/files/MsPdf.java
@@ -3,16 +3,12 @@ package br.com.schumaker.musashi.crawler.model.files;
 import br.com.schumaker.musashi.crawler.model.MsFile;
 import br.com.schumaker.musashi.crawler.model.MsReadable;
 import br.com.schumaker.musashi.crawler.model.readers.impl.MsPdfReaderFile;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Hudson Schumaker
  */
 public class MsPdf extends MsFile implements MsReadable {
-
-    @Autowired
-    private MsPdfReaderFile reader;
 
     public MsPdf() {}
 
@@ -21,13 +17,14 @@ public class MsPdf extends MsFile implements MsReadable {
     }
 
     @Override
-    public void process() {
+    public MsFile process() {
         super.setContent(read());
+        return this;
     }
 
     @Override
     public String read() {
-        return reader.read(getPath());
+        return new MsPdfReaderFile().read(getPath());
     }
 
     @Override
