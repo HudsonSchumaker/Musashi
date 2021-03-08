250ba7627b847ed6a92ecf328a4edca98b361f97
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:49:36 +0100

    Changed MsDbfile.

--- src/main/java/br/com/schumaker/musashi/crawler/model/db/MsDbFile.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/db/MsDbFile.java
@@ -6,6 +6,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/**
+ *
+ * @author Hudson Schumaker
+ */
 @Data
 @Document
 public class MsDbFile {
