0b08f90a02ba3dae657d2632ec00a25f746c6401
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-08 15:49:21 +0100

    Changed MsDbFileRepository.

--- src/main/java/br/com/schumaker/musashi/crawler/model/db/repositories/MsDbFileRepository.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/db/repositories/MsDbFileRepository.java
@@ -1,14 +1,18 @@
 package br.com.schumaker.musashi.crawler.model.db.repositories;
 
 import br.com.schumaker.musashi.crawler.model.db.MsDbFile;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+/**
+ *
+ * @author Hudson Schumaker
+ */
 @Repository
 public interface MsDbFileRepository extends MongoRepository<MsDbFile, String> {
 
-    List<MsDbFile> findByContentLike(String query, Pageable pageable);
+    Page<MsDbFile> findByNameLike(String query, Pageable pageable);
+    Page<MsDbFile> findByContentLike(String query, Pageable pageable);
 }
