f71eac24401feed85095a53e0d81deafe5255cc7
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-11 18:51:43 +0100

    Added MsReaderThreadFactory.

--- src/main/java/br/com/schumaker/musashi/crawler/model/readers/MsFileCommons.java
+++ src/main/java/br/com/schumaker/musashi/crawler/model/readers/MsFileCommons.java
@@ -1,5 +1,9 @@
 package br.com.schumaker.musashi.crawler.model.readers;
 
+/**
+ *
+ * @author Hudson Schumaker
+ */
 public class MsFileCommons {
     public static final int READ_SIZE = 6114;
     public static final int FILE_MAX_SIZE = 8388609; // 8MB
