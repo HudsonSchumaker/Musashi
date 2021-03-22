892fc3f13cd231c7ce79ce3a66af6310dc067bcf
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-23 00:04:33 +0100

    Changed MsCrawlerServiceImpl.java

--- src/main/java/br/com/schumaker/musashi/search/web/MsSearchController.java
+++ src/main/java/br/com/schumaker/musashi/search/web/MsSearchController.java
@@ -6,12 +6,10 @@ import br.com.schumaker.musashi.search.service.MsSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.ACCEPTED;
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  *
@@ -19,10 +17,19 @@ import static org.springframework.http.HttpStatus.ACCEPTED;
  */
 @RestController
 @RequestMapping("/search")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MsSearchController {
 
     @Autowired
     private MsSearchService service;
+
+    @GetMapping(value = "/{value}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MsPageDTO> getList(@PathVariable String value) {
+        MsSearchDTO dto = new MsSearchDTO();
+        dto.setQuery(value);
+
+        return ResponseEntity.status(OK).body(service.search(dto));
+    }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MsPageDTO> search(@RequestBody MsSearchDTO msSearchDTO) {
