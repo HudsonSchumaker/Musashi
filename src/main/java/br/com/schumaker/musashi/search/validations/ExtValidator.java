1b425965a6b54a09ac18fb77a99c6748e1902d49
Hudson Schumaker <hudson.schumaker@me.com>
2021-03-23 00:48:52 +0100

    Added ExtValidator

---
+++ src/main/java/br/com/schumaker/musashi/search/validations/ExtValidator.java
@@ -0,0 +1,12 @@
+package br.com.schumaker.musashi.search.validations;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ExtValidator implements ConstraintValidator<Ext, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return false;
+    }
+}
