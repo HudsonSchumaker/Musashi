ec9817d67eef78cb393255c3741370f81fe81722
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-07-11 22:03:25 +0200

    Update MusashiApplicationTests.java

--- src/test/java/br/com/schumaker/musashi/MusashiApplicationTests.java
+++ src/test/java/br/com/schumaker/musashi/MusashiApplicationTests.java
@@ -3,6 +3,10 @@ package br.com.schumaker.musashi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ *
+ * @author Hudson Schumaker
+ */
 @SpringBootTest
 class MusashiApplicationTests {
 
